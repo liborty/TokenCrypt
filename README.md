1daed55aa8ca04d65612b1d6846472a9180d94dc
liborty <libors@gmail.com>
2019-12-23 22:57:56 +1000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,77 @@
+# TokenCrypt
+
+A simple utility to encrypt and decrypt multiple security tokens, or any files.
+Tested under Linux. High security without onerous complications.
+
+### Outline
+
+Internet security tokens are usually 32,64 or more hexadecimal characters. 
+They are increasingly used to facilitate secure access to various 
+Applications Programming Interfaces (APIs) over the internet. Being plain text,
+they are easily transmitted but they need to be stored securely, that means encrypted.
+
+The heart of TokenCrypt is `symcrypt.c` which does fast symmetric 
+encryption or decryption of any type of file of any length. 
+
+Then there are a few 
+bash helper scripts to generate keys, to convert .hex files to binary and back and
+to automate the encryption of all files with .hex extension and the decryption of
+all files with .scr extension (in the current directory).
+
+If your token(s) consist of several parts, perhaps separated by a dash, 
+it is best to split them into individual files, each containing only the hexadecimal characters.
+
+In a Linux pipe, the hexadecimal token files (*.hex) are first converted to binary, 
+which halves them in length. Then the binary encryptor is applied.
+Decryption is a simple inverse of this process. 
+
+It is possible to naively encrypt/decrypt directly the hexadecimal files/tokens
+but this is sub-optimal, as the encrypted files are twice as long without any gains
+in security. In fact, it is probably a pessimisation of security as well.
+
+### symcrypt.c ###
+
+Stand-alone program written in C which takes keyfile, infile and outfile as arguments. 
+Outfile and infile can be omitted, in which case stdout and stdin are used. 
+This form is particularly convenient for use in a Unix/Linux pipe. 
+For the same reason, the program operates silently. Error messages are sent to stderr.
+Symcrypt is a general utility to encrypt/decrypt any files.
+
+**Installation:** 
+
+Download or clone this directory, cd into it and then:
+
+clang (or gcc) symcrypt.c -o symcrypt  
+chmod 700 symcrypt  
+mv symcrypt some/bin/directory/in-your-path
+
+**Operation:**
+
+cd directory/with/your/hextokens 
+ 
+establish the largest ot typical size of your token files, e.g.:  
+`stat -c%s myfile.hex`  
+`binkeygen size > keyfile`  
+Hide the keyfile somewhere safe and secure but note its path as you will need it.
+  
+`hexecrypt keyfile` or just `hexecrypt`  
+Calling hexecrypt without the keyfile argument will automatically generate 
+a new keyfile of a fixed predetermined length (8192 bits). 
+The default length can easily be changed in the hexecrypt bash script.
+  
+In any case, all `*.hex` files will be encrypted into `*.scr` files and can 
+now be deleted (at your own risk). It is strongly recommended that you test your
+ability to use TokenCrypt safely and confidently, converting in both directions and
+comparing to the original with `diff` before you start deleting any files. 
+In particular, if you lose your keyfile, you will not be able to decrypt and thus any
+deleted originals will be lost too.
+
+`hexdcrypt keyfile`  
+decrypts, recreating all the original `*.hex` files.
+
+
+  
+
+
+
+
---
+++ symcrypt.c
@@ -0,0 +1,115 @@
+#include "stdio.h"
+
+int main(int argc, char *argv[])
+{
+  FILE *fin1, *fin2, *fout;
+  char *progname = argv[0], *filein1, *filein2,  *fileout; 
+  int c1,c2;
+  unsigned char uc;
+
+  switch (argc)
+    {
+    case 4:
+      fileout = argv[3];
+      if ((fout = fopen(fileout,"wb")) == NULL)
+	    { fprintf(stderr,"%s: failed to open output file %s\n", progname,fileout);
+	    return(1);
+	    }
+      filein2 = argv[2];
+      if ((fin2 = fopen(filein2,"rb")) == NULL)
+	    { fprintf(stderr,"%s: failed to open input file %s\n", progname,filein2);
+	      fclose(fout);
+	      return(1);
+	    }
+      filein1 = argv[1];
+      if ((fin1 = fopen(filein1,"rb")) == NULL)
+	    { fprintf(stderr,"%s: failed to open input keyfile %s\n", progname,filein1);
+    	  fclose(fout);
+    	  fclose(fin2);
+    	  return(1);
+    	}
+  //  printf("%s %s %s %s running...\n",progname,filein1,filein2,fileout);
+      break;
+    case 3:
+      if ((fout = fopen("/dev/stdout","wb")) == NULL)
+	    { fprintf(stderr,"%s: failed to open /dev/stdout\n", progname);
+	    return(1);
+	    }
+      filein2 = argv[2];
+      if ((fin2 = fopen(filein2,"rb")) == NULL)
+	    { fprintf(stderr,"%s: failed to open input file %s\n", progname,filein2);
+	      fclose(fout);
+	      return(1);
+	    }
+      filein1 = argv[1];
+      if ((fin1 = fopen(filein1,"rb")) == NULL)
+	    { fprintf(stderr,"%s: failed to open input keyfile %s\n", progname,filein1);
+    	  fclose(fout);
+    	  fclose(fin2);
+    	  return(1);
+    	}
+    break;
+    case 2:
+      if ((fout = fopen("/dev/stdout","wb")) == NULL)
+	    { fprintf(stderr,"%s: failed to open /dev/stdout\n", progname);
+	      return(1);
+	    }
+      if ((fin2 = fopen("/dev/stdin","rb")) == NULL)
+	    { fprintf(stderr,"%s: failed to open /dev/stdin\n", progname);
+	      fclose(fout);
+	      return(1);
+	    }
+      filein1 = argv[1];
+      if ((fin1 = fopen(filein1,"rb")) == NULL)
+	    { fprintf(stderr,"%s: failed to open input keyfile %s\n", progname,filein1);
+    	  fclose(fout);
+    	  fclose(fin2);
+    	  return(1);
+    	}
+    break;
+    default:
+      fprintf(stderr,"usage: %s keyfile [infile [outfile]]\nomitted files mean stdin/out, e.g. in a pipe: .. | %s keyfile | ..\n",progname,progname);
+      return(1);
+    }
+   
+   while ((c2 = fgetc(fin2)) != EOF)
+     {
+       if ((c1 = fgetc(fin1)) == EOF) 
+	     {
+	       if (! feof(fin1))
+    	     {
+    	       fprintf(stderr,"%s: error in reading keyfile\n", progname);
+    	       fclose(fout);
+    	       return(1);
+	         } 
+           if (fseek( fin1, 0, SEEK_SET ))
+            {
+               fprintf(stderr,"%s: keyfile failed to seek to the beginning\n", progname);
+               fclose(fout);
+    	       return(1);
+            }
+    	   if ((c1 = fgetc(fin1)) == EOF) 
+    	     {
+    	       fprintf(stderr,"%s: error re-reading keyfile, or it is empty!\n", progname);
+    	       fclose(fout);
+    	       return(1);
+    	     } 
+    	 }
+       uc = ((unsigned char)c1)^((unsigned char)c2);
+       if (fputc(uc,fout) == EOF) 
+    	 {
+    	   fprintf(stderr,"%s: error in output\n", progname);
+    	   return(1);
+    	 } 
+      }
+   if (! feof(fin2))
+     fprintf(stderr,"%s: error in reading inputfile\n", progname);
+   if (fclose(fin2) == EOF)
+     fprintf(stderr,"%s: incorrectly closed inputfile\n", progname);
+   if (fclose(fin1) == EOF)
+     fprintf(stderr,"%s: incorrectly closed keyfile\n", progname);
+   if (fclose(fout) == EOF)
+     fprintf(stderr,"%s: incorrectly closed output\n", progname);
+   // fprintf(stderr,"%s %s finished\n",progname,filein1);
+   return(0);
+}
