a585cd99abd3569fb6945e05951f18304ee72b72
L <457124+liborty@users.noreply.github.com>
2021-11-28 00:05:27 +1000

    hexcheck & hexify fully buffered

--- hexify.c
+++ hexify.c
@@ -1,16 +1,20 @@
 #include "stdio.h"
 #include <unistd.h>
-
+#define BUFSIZE 1024
 // reads binary from stdin using a small buffer,
 // writes hex characters to stdout
 int main (void) {
-  unsigned char bufin[1024];  
+  unsigned char bufin[BUFSIZE];
+  char bufout[2*BUFSIZE]; 
+  char *bufoutadr;   
   int numread, i;
-
-  while ((numread = read(0, bufin, 1024)) > 0) {
+  while ((numread = read(0,bufin,BUFSIZE)) > 0) {
+    bufoutadr = bufout; // reset to the beginning
     for (i = 0; i < numread; i++) {
-      printf("%02x", bufin[i]);
+      sprintf(bufoutadr,"%02x", bufin[i]);
+      bufoutadr += 2;
     }
+    write(1,bufout,numread*2);
   }
  return 0;
 }
